3c630a14fa94c006b3ac42a56e3cafd3eba04c49
ni-c <willi@roamsys.com>
2012-09-03 23:14:07 +0200

    Updated database model

--- Config/Schema/photocake.sql
+++ Config/Schema/photocake.sql
@@ -1,26 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 30, 2012 at 10:58 PM
--- Server version: 5.5.24
--- PHP Version: 5.3.10-1ubuntu3.2
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Database: `photocake`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cameramodelnames`
---
-
-CREATE TABLE IF NOT EXISTS `cameramodelnames` (
+DROP TABLE IF EXISTS `cameramodelnames`;
+CREATE TABLE `cameramodelnames` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The name of the camera',
   `created` datetime NOT NULL COMMENT 'Created timestamp',
@@ -29,13 +11,8 @@ CREATE TABLE IF NOT EXISTS `cameramodelnames` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-CREATE TABLE IF NOT EXISTS `categories` (
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `name` varchar(16) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the Category',
   `slug` varchar(16) COLLATE utf8_unicode_ci NOT NULL COMMENT 'URL-Slug of the Category',
@@ -45,13 +22,8 @@ CREATE TABLE IF NOT EXISTS `categories` (
   UNIQUE KEY `name` (`name`,`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE IF NOT EXISTS `comments` (
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `photo_id` int(10) unsigned NOT NULL COMMENT 'Foreign key to posts',
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The name of the commenter',
@@ -63,13 +35,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `exposureprograms`
---
-
-CREATE TABLE IF NOT EXISTS `exposureprograms` (
+DROP TABLE IF EXISTS `exposureprograms`;
+CREATE TABLE `exposureprograms` (
   `id` int(11) NOT NULL COMMENT 'The primary key',
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The name of the Exposure Program',
   `created` datetime NOT NULL COMMENT 'Created timestamp',
@@ -78,23 +45,19 @@ CREATE TABLE IF NOT EXISTS `exposureprograms` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `exposureprograms` VALUES (0, 'Not defined', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (1, 'Manual', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (2, 'Normal program', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (3, 'Aperture priority', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (4, 'Shutter priority', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (5, 'Creative program', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (6, 'Action program', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (7, 'Portrait mode', NOW(), NOW());
-INSERT INTO `exposureprograms` VALUES (8, 'Landscape mode', NOW(), NOW());
+INSERT INTO `exposureprograms` (`id`, `name`, `created`, `modified`) VALUES
+(0, 'Not defined', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(1, 'Manual', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(2, 'Normal program', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(3, 'Aperture priority', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(4, 'Shutter priority', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(5, 'Creative program', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(6, 'Action program', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(7, 'Portrait mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(8, 'Landscape mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `flashes`
---
-
-CREATE TABLE IF NOT EXISTS `flashes` (
+DROP TABLE IF EXISTS `flashes`;
+CREATE TABLE `flashes` (
   `id` int(11) NOT NULL COMMENT 'The primary key',
   `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The type of flash',
   `created` datetime NOT NULL COMMENT 'Created timestamp',
@@ -103,36 +66,32 @@ CREATE TABLE IF NOT EXISTS `flashes` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `flashes` VALUES (0, 'Flash did not fire', NOW(), NOW());
-INSERT INTO `flashes` VALUES (1, 'Flash fired', NOW(), NOW());
-INSERT INTO `flashes` VALUES (5, 'Strobe return light not detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (7, 'Strobe return light detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (9, 'Flash fired, compulsory flash mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (13, 'Flash fired, compulsory flash mode, return light not detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (15, 'Flash fired, compulsory flash mode, return light detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (16, 'Flash did not fire, compulsory flash mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (24, 'Flash did not fire, auto mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (25, 'Flash fired, auto mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (29, 'Flash fired, auto mode, return light not detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (31, 'Flash fired, auto mode, return light detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (32, 'No flash function', NOW(), NOW());
-INSERT INTO `flashes` VALUES (65, 'Flash fired, red-eye reduction mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (69, 'Flash fired, red-eye reduction mode, return light not detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (71, 'Flash fired, red-eye reduction mode, return light detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (73, 'Flash fired, compulsory flash mode, red-eye reduction mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (77, 'Flash fired, compulsory flash mode, red-eye reduction mode, return light not detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (79, 'Flash fired, compulsory flash mode, red-eye reduction mode, return light detected', NOW(), NOW());
-INSERT INTO `flashes` VALUES (89, 'Flash fired, auto mode, red-eye reduction mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (93, 'Flash fired, auto mode, return light not detected, red-eye reduction mode', NOW(), NOW());
-INSERT INTO `flashes` VALUES (95, 'Flash fired, auto mode, return light detected, red-eye reduction mode ', NOW(), NOW());
+INSERT INTO `flashes` (`id`, `name`, `created`, `modified`) VALUES
+(0, 'Flash did not fire', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(1, 'Flash fired', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(5, 'Strobe return light not detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(7, 'Strobe return light detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(9, 'Flash fired, compulsory flash mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(13, 'Flash fired, compulsory flash mode, return light not detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(15, 'Flash fired, compulsory flash mode, return light detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(16, 'Flash did not fire, compulsory flash mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(24, 'Flash did not fire, auto mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(25, 'Flash fired, auto mode', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(29, 'Flash fired, auto mode, return light not detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(31, 'Flash fired, auto mode, return light detected', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(32, 'No flash function', '2012-09-03 21:11:57', '2012-09-03 21:11:57'),
+(65, 'Flash fired, red-eye reduction mode', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(69, 'Flash fired, red-eye reduction mode, return light not detected', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(71, 'Flash fired, red-eye reduction mode, return light detected', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(73, 'Flash fired, compulsory flash mode, red-eye reduction mode', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(77, 'Flash fired, compulsory flash mode, red-eye reduction mode, return light not detected', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(79, 'Flash fired, compulsory flash mode, red-eye reduction mode, return light detected', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(89, 'Flash fired, auto mode, red-eye reduction mode', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(93, 'Flash fired, auto mode, return light not detected, red-eye reduction mode', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(95, 'Flash fired, auto mode, return light detected, red-eye reduction mode ', '2012-09-03 21:11:58', '2012-09-03 21:11:58');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `lenses`
---
-
-CREATE TABLE IF NOT EXISTS `lenses` (
+DROP TABLE IF EXISTS `lenses`;
+CREATE TABLE `lenses` (
   `id` int(11) NOT NULL COMMENT 'The primary key',
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The name of the lens',
   `created` datetime NOT NULL COMMENT 'Created timestamp',
@@ -141,19 +100,15 @@ CREATE TABLE IF NOT EXISTS `lenses` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `lenses` VALUE (0, 'walimex Pro 8mm f/3.5 Fish-Eye', NOW(), NOW());
-INSERT INTO `lenses` VALUE (29, 'Canon EF 50mm 1.8 II', NOW(), NOW());
-INSERT INTO `lenses` VALUE (153, 'Tamron 18-200mm f/3,5-6,3 XR Di II', NOW(), NOW());
-INSERT INTO `lenses` VALUE (156, 'Tamron 70-300mm f/4-5.6 Di VC USD', NOW(), NOW());
+INSERT INTO `lenses` (`id`, `name`, `created`, `modified`) VALUES
+(0, 'walimex Pro 8mm f/3.5 Fish-Eye', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(29, 'Canon EF 50mm 1.8 II', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(153, 'Tamron 18-200mm f/3,5-6,3 XR Di ', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(156, 'Tamron 70-300mm f/4-5.6 Di VC US', '2012-09-03 21:11:58', '2012-09-03 21:11:58');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `meteringmodes`
---
-
-CREATE TABLE IF NOT EXISTS `meteringmodes` (
-  `id` int(11) NOT NULLCOMMENT 'The primary key',
+DROP TABLE IF EXISTS `meteringmodes`;
+CREATE TABLE `meteringmodes` (
+  `id` int(11) NOT NULL COMMENT 'The primary key',
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The name of the Metering Mode',
   `created` datetime NOT NULL COMMENT 'Created timestamp',
   `modified` datetime NOT NULL COMMENT 'Modified timestamp',
@@ -161,22 +116,18 @@ CREATE TABLE IF NOT EXISTS `meteringmodes` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `meteringmodes` VALUES (0, 'Unknown', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (1, 'Average', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (2, 'Center-weighted average', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (3, 'Spot', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (4, 'Multi-spot', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (5, 'Pattern', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (6, 'Partial', NOW(), NOW());
-INSERT INTO `meteringmodes` VALUES (255, 'Other', NOW(), NOW());
+INSERT INTO `meteringmodes` (`id`, `name`, `created`, `modified`) VALUES
+(0, 'Unknown', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(1, 'Average', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(2, 'Center-weighted average', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(3, 'Spot', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(4, 'Multi-spot', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(5, 'Pattern', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(6, 'Partial', '2012-09-03 21:11:58', '2012-09-03 21:11:58'),
+(255, 'Other', '2012-09-03 21:11:58', '2012-09-03 21:11:58');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `options`
---
-
-CREATE TABLE IF NOT EXISTS `options` (
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE `options` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `key` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The key',
   `value` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The value',
@@ -186,13 +137,8 @@ CREATE TABLE IF NOT EXISTS `options` (
   UNIQUE KEY `key` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `photos`
---
-
-CREATE TABLE IF NOT EXISTS `photos` (
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE `photos` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `category_id` int(11) unsigned NOT NULL COMMENT 'The category of the photo',
   `cameramodelname_id` int(11) unsigned DEFAULT NULL COMMENT 'EXIF for Camera Model Name',
@@ -217,13 +163,8 @@ CREATE TABLE IF NOT EXISTS `photos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `photos_tags`
---
-
-CREATE TABLE IF NOT EXISTS `photos_tags` (
+DROP TABLE IF EXISTS `photos_tags`;
+CREATE TABLE `photos_tags` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `photo_id` int(10) unsigned NOT NULL COMMENT 'The id of the photo',
   `tag_id` int(10) unsigned NOT NULL COMMENT 'The id of the tag',
@@ -232,13 +173,8 @@ CREATE TABLE IF NOT EXISTS `photos_tags` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tags`
---
-
-CREATE TABLE IF NOT EXISTS `tags` (
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The primary key',
   `name` varchar(16) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the Tag',
   `slug` varchar(16) COLLATE utf8_unicode_ci NOT NULL COMMENT 'URL-Slug of the Tag',
